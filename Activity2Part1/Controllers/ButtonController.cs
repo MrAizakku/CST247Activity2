9580d6dd47391317dc9c4f2066b711f984cb5341
Isaac Tucker <isaac.takka@gmail.com>
2020-11-22 00:05:33 -0700

    Initial commit

---
+++ Activity2Part1/Controllers/ButtonController.cs
@@ -0,0 +1,41 @@
+﻿using Activity2Part1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Activity2Part1.Controllers
+{
+    public class ButtonController : Controller
+    {
+        static public List<ButtonModel> buttons;
+        Random rand = new Random();
+        // GET: Button
+        public ActionResult Index()
+        {
+            buttons = getButtonList();
+            return View("Button", buttons);
+        }
+
+        public ActionResult OnButtonClick(string mine)
+        {
+            int buttonNumber = Int32.Parse(mine);
+            buttons[buttonNumber].State = !buttons[buttonNumber].State;
+            return View("Button", buttons);
+        }
+
+        private List<ButtonModel> getButtonList()
+        {
+            buttons = new List<ButtonModel>();
+            for (int i = 0; i < 25; i++)
+            {
+                if (rand.Next(10) > 5)
+                    buttons.Add(new ButtonModel(true));
+                else
+                    buttons.Add(new ButtonModel(false));
+            }
+            return buttons;
+        }
+    }
+}
---
+++ Activity2Part1/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using Activity2Part1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Activity2Part1.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            var users = getUserList();
+            return View("Test", users);
+        }
+
+        public List<UserModel> getUserList()
+        {
+            List<UserModel> users = new List<UserModel>();
+            users.Add(new UserModel("Jon Abbot", "Vampire@hotmail.com", "555-444-3333"));
+            users.Add(new UserModel("Trevenor Belmont", "VampireKiller@hotmail.com", "555-443-3333"));
+            users.Add(new UserModel("ALucard Teppes", "Vampir3@hotmail.com", "555-434-3343"));
+            return users;
+        }
+    }
+}
